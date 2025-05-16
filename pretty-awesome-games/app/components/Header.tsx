432c8a81d181598571f4d5905ef780b59e503c6f
Ludwik Bacmaga <luddzik@gmail.com>
2025-05-16 22:31:27 +0200

    Update Header.tsx

--- pretty-awesome-games/app/components/Header.tsx
+++ pretty-awesome-games/app/components/Header.tsx
@@ -44,7 +44,7 @@ const Header: React.FC<HeaderProps> = ({ showNavLinks = true }) => {
       <div className={styles.container}>
         <Link href="/" className={styles.logoContainer} onClick={() => scrollToSection("top")}>
           <div>
-            <Image src="../favicon.svg" alt="Logo" width={64} height={64} />
+            <Image src="../favicon.svg" alt="Logo" width={96} height={96} />
           </div>
           <div className={styles.logoText}>
             <span className={styles.logoMainText}></span>
