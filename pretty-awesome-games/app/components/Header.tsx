75010189bac43b849dba3a9f4d35b9e0bab6947f
Ludwik Bacmaga <luddzik@gmail.com>
2025-06-11 19:56:25 +0200

    logo sizing update

--- pretty-awesome-games/app/components/Header.tsx
+++ pretty-awesome-games/app/components/Header.tsx
@@ -44,7 +44,7 @@ const Header: React.FC<HeaderProps> = ({ showNavLinks = true }) => {
       <div className={styles.container}>
         <Link href="/" className={styles.logoContainer} onClick={() => scrollToSection("top")}>
           <div>
-            <Image src="../logo_long.svg" alt="Logo" width={64} height={64} />
+            <Image src="../logo_long.svg" alt="Logo" width={50} height={50} />
           </div>
           <div className={styles.logoText}>
             <span className={styles.logoMainText}></span>
--- pretty-awesome-games/app/components/Hero.tsx
+++ pretty-awesome-games/app/components/Hero.tsx
@@ -24,7 +24,7 @@ const Hero: React.FC = () => {
           className={styles.content}
         >
           <div className="styles.logo">
-            <Image src="../logo_long.png" alt="Logo" width={256} height={256} />
+            <Image src="../logo_long.png" alt="Logo" width={312} height={312} />
           </div>
         
           <motion.button
