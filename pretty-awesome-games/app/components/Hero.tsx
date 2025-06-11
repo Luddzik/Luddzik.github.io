d9c84dbc600065753759742feee63ed9d2c5f0a4
Ludwik Bacmaga <luddzik@gmail.com>
2025-06-11 20:02:08 +0200

    Update Hero.tsx

--- pretty-awesome-games/app/components/Hero.tsx
+++ pretty-awesome-games/app/components/Hero.tsx
@@ -24,7 +24,7 @@ const Hero: React.FC = () => {
           className={styles.content}
         >
           <div className="styles.logo">
-            <Image src="../logo_long.png" alt="Logo" width={512} height={312} />
+            <Image src="../logo_long.png" alt="Logo" width={400} height={312} />
           </div>
         
           <motion.button
